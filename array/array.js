ed8b9b50efd341e430f3115caded8fe1fb87f6c6
Andrés Gutiérrez <andres_mau.gutierrez@uao.edu.co>
2022-12-04 19:06:50 +0000

    Construyendo un array como clase

---
+++ array/array.js
@@ -0,0 +1,19 @@
+// Generar un array con una clase
+class MyArray {
+    constructor(){
+        this.length = 0;
+        this.data = {}
+    }
+
+    get(index){
+        return this.data[index];
+    }
+
+    push(item){
+        this.data[this.length] = item;
+        this.length ++;
+        return this.data;
+    }
+}
+
+const myArray = new MyArray();
